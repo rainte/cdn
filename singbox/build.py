fd1ee213e559f01d921efbfa4b2f587d5efe6adc
Sean <sxfmao@163.com>
2024-04-18 11:37:06 +0800

    commit

--- singbox/build.py
+++ singbox/build.py
@@ -6,9 +6,11 @@ import os
 def singbox(dir):
     pc = dir + "/pc"
     mobile = dir + "/mobile"
+    os.makedirs(pc, exist_ok=True)
+    os.makedirs(mobile, exist_ok=True)
 
     default = '{"dns":{"servers":[{"tag":"google","address":"tls://8.8.8.8"},{"tag":"local","address":"223.5.5.5","detour":"direct"}],"rules":[{"outbound":"any","server":"local"}]},"inbounds":[{"type":"tun","inet4_address":"172.19.0.1/30","inet6_address":"fdfe:dcba:9876::1/126","auto_route":true,"sniff":true}],"outbounds":[{"type":"direct","tag":"direct"},{"type":"dns","tag":"dns-out"}],"route":{"rules":[{"protocol":"dns","outbound":"dns-out"},{"geoip":"private","outbound":"direct"}],"auto_detect_interface":true}}'
-    map = {
+    urls = {
         "config1": "https://www.gitlabip.xyz/Alvin9999/pac2/master/singbox/1/config.json",
         "config2": "https://gitlab.com/free9999/ipupdate/-/raw/master/singbox/config.json",
         "config3": "https://www.githubip.xyz/Alvin9999/pac2/master/singbox/config.json",
@@ -17,9 +19,7 @@ def singbox(dir):
 
     config = json.loads(default)
     outbounds = config["outbounds"]
-    os.makedirs(pc, exist_ok=True)
-    os.makedirs(mobile, exist_ok=True)
-    for key, value in map.items():
+    for key, value in urls.items():
         response = requests.get(value, verify=False)
         if response.status_code == 200:
             data = response.json()
